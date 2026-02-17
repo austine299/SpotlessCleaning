5c223e23b3b3efac89de3ba9a37d6fee5faadccf
imah ugochukwu <ugochukwuaugustine299@gmail.com>
2026-02-17 10:21:05 +0100

    login update

--- src/components/AdminRegister.js
+++ src/components/AdminRegister.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { registerAdmin } from "../data/AdminStorage";
-
 export default function AdminRegister() {
   const navigate = useNavigate();
 
